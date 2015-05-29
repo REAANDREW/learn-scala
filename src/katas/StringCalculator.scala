86d87148f5f9fd9edcd992c5653b37eeb89924b2
Andrew <email@andrewrea.co.uk>
2015-05-29 08:37:40 +0100

    support a defined delimiter

--- src/katas/StringCalculator.scala
+++ src/katas/StringCalculator.scala
@@ -1,8 +1,22 @@
 package katas
 
+import scala.language.postfixOps
+import scala.annotation.tailrec
+
+class NumbersExtractor{
+  def extract(numbers:String) = {
+      val delimiter = numbers.charAt(2)
+      val value = numbers.split("\n")(1)
+      value.split(delimiter) 
+  }
+}
+
 class StringCalculator{
 
+  def extractor : NumbersExtractor = new NumbersExtractor()
+
   def addMultiple(numbers: List[String]) : Int = {
+    @tailrec
     def sumAccumulator(numbers: List[String], accum: Int) : Int = {
       numbers match {
         case Nil => accum
@@ -13,8 +27,18 @@ class StringCalculator{
     sumAccumulator(numbers, 0)
   }
 
-  def Add(numbers: String) : Int = {
-    if(numbers.isEmpty) 0 
-    else addMultiple(numbers.split(Array(',','\n')).toList)
-  }
+  def Add(numbers: String) = 
+    numbers match{
+      case n:String if n nonEmpty => NonEmptyInput(n)
+      case _ => 0
+    }
+
+  def NonEmptyInput(numbers:String) = 
+    numbers match {
+      case n:String if n startsWith "//" => DefinedDelimiter(numbers)
+      case _ => addMultiple(numbers.split(Array(',','\n')).toList)
+    }
+
+  def DefinedDelimiter(numbers:String) = 
+    addMultiple(extractor.extract(numbers).toList)
 }
--- test/StringCalculatorSpec.scala
+++ test/StringCalculatorSpec.scala
@@ -3,12 +3,24 @@ package test
 import org.specs2.mutable._
 import katas._
 
+class DelimiterExtractorSpec extends Specification{
+  "This is a specification for the DelimiterExtractor".txt
+
+  def extractor: NumbersExtractor = new NumbersExtractor()
+
+  "The 'Extract(numbers: String)' method can handle" >> {
+    "Defined delimiter" should{
+      extractor extract "//;\n1;2" mustEqual(Array("1","2"))
+    }
+  }
+}
+
 class StringCalculatorSpec extends Specification{
   "This is a specification for the String Calculator Kata".txt
 
   def calculator: StringCalculator = new StringCalculator()
 
-  "The 'Add(numbers: string)' method can handle" >> {
+  "The 'Add(numbers: String)' method can handle" >> {
 
     "0 numbers" should{
       calculator Add "" mustEqual(0)
@@ -35,16 +47,21 @@ class StringCalculatorSpec extends Specification{
     //syntax of the range and also the out of the box
     //functions attached to Iterables! :-)
     //
-    "100 numbers" should{
-      val range = 1 until 100
-      val expected = range sum;
-      val input = range mkString ","
-      calculator Add (input) mustEqual(expected)
+    "1,..,100" should{
+      "return 4950" in {
+        val range = 1 until 100
+        val expected = range sum;
+        val input = range mkString ","
+        calculator Add (input) mustEqual(expected)
+      }
     }
 
     "A set of numbers with a new line delimiter present" should{
       calculator Add "1\n2,3" mustEqual 6
     }
 
+    "Allow for a defined delimiter" should{ 
+      calculator Add "//;\n1;2" mustEqual 3
+    }
   }
 }
